efc9cb23b5f3aadb9302c039180f45c9df7ad8e4
shohel rana <shohel@ocody.com>
2018-03-03 22:13:03 +0600

    update basic form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,8 +19,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    // FormsModule,
+    // ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/form-validation/form-validation.module.ts
+++ src/app/form-validation/form-validation.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { FormValidationRoutingModule } from './form-validation-routing.module';
 import { BasicFormComponent } from './basic-form/basic-form.component';
 import { FormValidationComponent } from './form-validation.component';
@@ -7,6 +9,8 @@ import { FormValidationComponent } from './form-validation.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     FormValidationRoutingModule
   ],
   declarations: [FormValidationComponent, BasicFormComponent]
