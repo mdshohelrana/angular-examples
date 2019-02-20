3474142dbf0fa486acdbf46dd7712f067dc75c11
unknown <ferozahmed.cse26@gmail.com>
2019-02-20 20:31:44 +0600

    added nested validation

--- src/app/form-validation/master-child-form/master-child-form.component.ts
+++ src/app/form-validation/master-child-form/master-child-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './Product.model';
 
 @Component({
   selector: 'app-master-child-form',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterChildFormComponent implements OnInit {
 
+  private productList: Product[] = new Array();
+
+  private customerOptions: any = {
+    "name": {
+      'size': {
+        'min': 3,
+        'max': 20,
+        'message': 'Customer Name is required to be between 3 and 20 characters.'
+      },
+      "required": {
+        "message": "Customer Name required",
+      },
+    },
+    "mobileNumber": {
+      'size': {
+        'min': 11,
+        'max': 11,
+        'message': 'Customer Name is should to be 11 characters.'
+      },
+      "required": {
+        "message": "Mobile Number required",
+      },
+    },
+    "address": {
+      "required": {
+        "message": "Address required",
+      },
+    },
+    "productName": {
+      "required": {
+        "message": "Product Name required",
+      },
+    },
+    "productPrice": {
+      "required": {
+        "message": "Product Price required",
+      },
+    },
+    "productQuantity": {
+      "required": {
+        "message": "Product Quantity required",
+      },
+    },
+    "total": {
+      "required": {
+        "message": "total required",
+      },
+    },
+    message: "tooltip"
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewProduct(form) {
+    console.log(form);
+    
+    let _validationResult = form.validate();
+    if(_validationResult.isValid) {
+      this.productList.push(new Product());
+    }
+    console.log(_validationResult);
+  }
+  addProduct(form) {
+    let _validationResult = form.validate();
+    if(_validationResult.isValid) {
+      this.productList.push(new Product());
+    }
+    console.log(_validationResult);
+  }
+  saveCustomer(form) {
+    let _validationResult = form.validate();
+    console.log(_validationResult);
+  }
+  resetCustomer(form) {
+    let _validationResult = form.reset();
+    console.log(_validationResult);
   }
 
 }
